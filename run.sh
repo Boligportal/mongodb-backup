5350d71058cc561861082ee1c75f994d09fa8fd2
Nikki Ashton <na@boligportal.dk>
2017-01-23 13:03:31 +0100

    Fix missing env in cronjob

--- run.sh
+++ run.sh
@@ -8,7 +8,12 @@ if [ -n "${INIT_BACKUP}" ]; then
     /backup.sh
 fi
 
-echo "${CRON_TIME} /backup.sh >> /mongo_backup.log 2>&1" > /crontab.conf
+echo "=> Preparing env vars for backup script running in cron"
+printenv | sed 's/^\(.*\)\=\(.*\)$/export \1\="\2"/g' > /root/cron_env.sh
+
+echo "=> Preparing crontab"
+echo "${CRON_TIME} . /root/cron_env.sh; /backup.sh >> /mongo_backup.log 2>&1" > /crontab.conf
 crontab  /crontab.conf
+
 echo "=> Running cron job"
 exec cron -f
